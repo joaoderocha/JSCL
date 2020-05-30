4b41e603b471f3aab78927fe57566cc9fd8fcc01
Camazaqui <jharocha@gmail.com>
2020-05-30 16:58:16 -0300

    feat: examples

--- src/folder2/index.js
+++ src/folder2/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = {
+  msg: 'olaaa',
   main,
 };
 
--- test/testes.test.js
+++ test/testes.test.js
@@ -1,8 +1,11 @@
 'use strict';
 
 const { test } = require('tap');
-const { msg } = require('../src/folder2');
+const { msg, main } = require('../src/folder2');
 
 test('heloooooo', (t) => {
   t.same(msg, 'olaaa');
+  main();
+
+  t.end();
 });
