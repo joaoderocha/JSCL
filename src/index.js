981e1b056242e8baf74d86ead5c87948498af642
Lucas Urzedo <lucasurzedo1@gmail.com>
2020-06-05 10:21:24 -0300

    feat: adiciona aplicação na aws

--- src/index.js
+++ src/index.js
@@ -1,9 +1,56 @@
 'use strict';
 
-const folder2 = require('./folder2');
-const msg = 'olaaa';
+const express = require("express");
+const app = express();
 
-module.exports = {
-  msg,
-  folder2,
-};
+var port = process.env.PORT || 3000;
+
+var operations = {
+  fatorial: function (n) {
+    if (n < 2)
+      return 1;
+    return operations.fatorial(n - 1) * n;
+  },
+  add: function (a, b) {
+    return a + b;
+  },
+  sub: function (a, b) {
+    return a - b;
+  }
+}
+
+app.use(express.json());
+
+app.get('/', function (req, res) {
+  res.end("Hello World!");
+});
+
+app.post("/operations/fatorial", function (req, res) {
+  const request = req.body;
+
+  if (request.fatorial < 0)
+    res.send("Erro");
+  else {
+    var result = { "fatorial": operations.fatorial(request.fatorial) }
+
+    res.json(result);
+  }
+});
+
+app.post("/operations/add", function (req, res) {
+  const request = req.body;
+
+  var result = { "soma": operations.add(request.soma[0], request.soma[1]) }
+
+  res.json(result);
+});
+
+app.post("/operations/sub", function (req, res) {
+  const request = req.body;
+
+  var result = { "sub": operations.sub(request.sub[0], request.sub[1]) }
+
+  res.json(result);
+});
+
+app.listen(port);
